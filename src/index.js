1daed2d50d501cd76cdbe811b1b73c14e2e25999
Neelam Verma <neelamv222@gmail.com>
2018-06-18 02:39:27 +0530

    created login form and sign up form

--- src/index.js
+++ src/index.js
@@ -4,5 +4,21 @@ import './assets/styles/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+
+const rootReducer = combineReducers({
+    form: formReducer,
+});
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+
 registerServiceWorker();
