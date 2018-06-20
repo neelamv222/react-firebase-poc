01da68e8027a6b5a895653869ca803d2f97ecdbf
Neelam Verma <neelamv222@gmail.com>
2018-06-20 23:54:04 +0530

    Basic firebase setup with our application

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-
-import { store } from './reducers/store/store';
 import HomeScreen from './screens/home';
 
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <div className="App">
-          <HomeScreen />
-        </div>
-      </Provider>
+      <div className="App">
+        <HomeScreen />
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,16 +3,11 @@ import ReactDOM from 'react-dom';
 import './assets/styles/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+import { store } from './reducers/store/store';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
-
-const rootReducer = combineReducers({
-    form: formReducer,
-});
-
-const store = createStore(rootReducer);
+import './firebase/firebase';
 
 ReactDOM.render(
     <Provider store={store}>
